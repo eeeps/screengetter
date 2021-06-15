35a6dc000f4dee27446b4cd47bdf93b7847b4440
Eric Portis <e@ericportis.com>
2021-06-14 17:37:43 -0700

    print things

---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  screengetter
+//
+//  Created by Eric Portis on 6/14/21.
+//
+
+import Foundation
--- screengetter/main.swift
+++ screengetter/main.swift
@@ -6,6 +6,27 @@
 //
 
 import Foundation
+import AppKit
 
-print("Hello, World!")
+func getDisplayBrightness() -> Float {
+
+    var brightness: Float = 1.0
+    var service: io_object_t = 1
+    var iterator: io_iterator_t = 0
+    let result: kern_return_t = IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IODisplayConnect"), &iterator)
+
+    if result == kIOReturnSuccess {
+
+        while service != 0 {
+            service = IOIteratorNext(iterator)
+            IODisplayGetFloatParameter(service, 0, kIODisplayBrightnessKey as CFString, &brightness)
+            IOObjectRelease(service)
+        }
+    }
+    return brightness
+}
+
+print("maximumExtendedDynamicRangeColorComponentValue: ", NSScreen.main?.maximumExtendedDynamicRangeColorComponentValue)
+print("maximumPotentialExtendedDynamicRangeColorComponentValue: ", NSScreen.main?.maximumPotentialExtendedDynamicRangeColorComponentValue)
+print("kIODisplayBrightnessKey: ", getDisplayBrightness())
 
